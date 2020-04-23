66914d5f5f62f46d0e9c32a222ace5e4d6cad07d
dreamwalker1999 <atharva.lord10@gmail.com>
2020-04-23 17:09:26 +0530

    Updates

---
+++ precache-manifest.edfd342047b17a2876a158929bed66c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f041726005a55e2275ac5240a6660ba9",
+    "url": "/react-clothing-app/index.html"
+  },
+  {
+    "revision": "a4c48d8fdcf365610242",
+    "url": "/react-clothing-app/static/css/main.74fc2201.chunk.css"
+  },
+  {
+    "revision": "c9d4a869fcf559e8a5cf",
+    "url": "/react-clothing-app/static/js/2.d8dc4ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clothing-app/static/js/2.d8dc4ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c48d8fdcf365610242",
+    "url": "/react-clothing-app/static/js/main.583ca415.chunk.js"
+  },
+  {
+    "revision": "7d3cfb383b867874f4b4",
+    "url": "/react-clothing-app/static/js/runtime-main.47864ec8.js"
+  }
+]);
